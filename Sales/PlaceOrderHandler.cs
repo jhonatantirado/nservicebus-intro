2874534911f421d31d2e1692fed3c2869ac47971
Jhonatan Tirado <jhonatantiradotiradodeep@gmail.com>
2018-12-04 21:05:18 -0500

    Lesson 5

--- Sales/PlaceOrderHandler.cs
+++ Sales/PlaceOrderHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Messages;
 using NServiceBus;
@@ -7,10 +8,16 @@ namespace Sales{
     public class PlaceOrderHandler :IHandleMessages<PlaceOrder>
     {
         static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
+        static Random random = new Random();
 
         public Task Handle(PlaceOrder message, IMessageHandlerContext context)
         {
             log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+
+            if (random.Next(0, 5) == 0)
+            {
+                throw new Exception("Oops");
+            }
 
             var orderPlaced = new OrderPlaced {
                 OrderId = message.OrderId
